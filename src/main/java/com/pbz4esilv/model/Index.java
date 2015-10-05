61d3595e76c0795f41890cef12b44d39abb75151
Pierre Marion <p.marion1@hotmail.com>
2015-10-05 11:24:55 +0200

    creation variable

--- src/main/java/com/pbz4esilv/model/Index.java
+++ src/main/java/com/pbz4esilv/model/Index.java
@@ -6,11 +6,13 @@ import java.util.List;
 public class Index {
     private String name;
     private Date date;
+    private double value;
     private List<Equity> equityList;
 
-    public Index(String name, Date date, List<Equity> equityList){
+    public Index(String name, Date date,double value, List<Equity> equityList){
         this.name = name;
         this.date = date;
+        this.value=value;
         this.equityList = equityList;
     }
 
