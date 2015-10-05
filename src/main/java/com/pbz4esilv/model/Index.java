82b654d8159cc930a79d3dac4e83a1b1ec808ccf
Pierre Marion <p.marion1@hotmail.com>
2015-10-05 10:51:08 +0200

    creation variable

--- src/main/java/com/pbz4esilv/model/Index.java
+++ src/main/java/com/pbz4esilv/model/Index.java
@@ -2,4 +2,17 @@ package com.pbz4esilv.model;
 
 
 public class Index {
+
+    private String indexname;
+    private String date;
+    private double value;
+
+    public Index(String indexname,String date, double value ) {
+
+        this.indexname = indexname ;
+        this.date = date;
+        this.value = value ;
+
+    }
 }
+
