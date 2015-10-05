900190fbb2ec813d4f69b0d4cd3814a187c83665
bjtbjt <benoit.harbeumont@gmail.com>
2015-10-05 10:33:45 +0200

    création des variables

--- src/test/java/com/pbz4esilv/model/EquityTest.java
+++ src/test/java/com/pbz4esilv/model/EquityTest.java
@@ -3,11 +3,15 @@ package com.pbz4esilv.model;
 import  junit.framework.Assert;
 import org.junit.Test;
 
+import java.util.Date;
+
 public class EquityTest {
 
     @Test
     public void testGetName() throws Exception {
-        Equity eq1 = new Equity("AXA",100);
+        Equity eq1;
+        Date d1= new Date() ;
+        eq1 = new Equity("AXA","axa",100,20,d1,10000 );
         Assert.assertEquals("AXA", eq1.getName());
     }
 
