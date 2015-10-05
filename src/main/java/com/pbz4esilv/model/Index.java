2d186497978b1a64f423568a4e21ab8be299ae8f
arthur <arthurprvst@yahoo.com>
2015-10-05 11:25:46 +0200

    variables index

--- src/main/java/com/pbz4esilv/model/Index.java
+++ src/main/java/com/pbz4esilv/model/Index.java
@@ -4,4 +4,7 @@ package com.pbz4esilv.model;
  * Created by Alessandro on 05/10/2015.
  */
 public class Index {
+    private String nom;
+    private double valeur;
+    private String[] Constituants;
 }
