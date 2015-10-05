a1101d4b91aecb7e6bc80a413dd5f6dcf6c8b393
absaboss <absalon.deel@laposte.net>
2015-10-05 10:49:33 +0200

    sqss

--- src/main/java/com/pbz4esilv/model/Index.java
+++ src/main/java/com/pbz4esilv/model/Index.java
@@ -1,5 +1,17 @@
 package com.pbz4esilv.model;
 
+import java.util.Date;
+import java.util.List;
 
 public class Index {
+    private String name;
+    private Date date;
+    private List<Equity> equityList;
+
+    public Index(String name, Date date, List<Equity> equityList){
+        this.name = name;
+        this.date = date;
+        this.equityList = equityList;
+    }
+
 }
