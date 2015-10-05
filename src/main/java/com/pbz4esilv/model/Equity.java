2c0224682f7603c570b8f29904f2706ba69a1eb6
bjtbjt <benoit.harbeumont@gmail.com>
2015-10-05 10:09:22 +0200

    création des variables

--- src/main/java/com/pbz4esilv/model/Equity.java
+++ src/main/java/com/pbz4esilv/model/Equity.java
@@ -1,8 +1,12 @@
 package com.pbz4esilv.model;
 
+import java.util.Date;
+
 public class Equity {
 
     private String name;
-    private String ticket;
-    private int price;
+    private String ticker;
+    private double price;
+    private double volatility;
+    private Date dateValuation ;
 }
