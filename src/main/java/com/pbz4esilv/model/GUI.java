b703d05896671fc1a2b42d37a4245d468cacac35
ChiChi <chironromain@gmail.com>
2015-11-01 16:50:05 +0100

    Ajout d'un bouton LOAD et d'un menu déroulant qui recevra les différents indices

--- src/main/java/com/pbz4esilv/model/GUI.java
+++ src/main/java/com/pbz4esilv/model/GUI.java
@@ -2,8 +2,17 @@ package com.pbz4esilv.model;
 
 import oracle.jrockit.jfr.JFR;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Container;
+
+import javax.swing.BorderFactory;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 /**
  * Created by ChiChi on 05/10/2015.
@@ -26,6 +35,37 @@ public class GUI extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Rend la fenêtre visible
         this.setVisible(true);
+
+        //On instancie un objet JPanel qui contiendra nos boutons et autres
+        JPanel panel = new JPanel();
+        //new GridLayout(0,1)
+        //pan.setBackground(Color.BLACK);
+        this.setContentPane(panel);
+
+        Border border = BorderFactory.createTitledBorder("Selection");
+        panel.setBorder(border);
+
+        //On ajoute une Combobox
+        JComboBox combo = new JComboBox();
+        combo.setPreferredSize(new Dimension(100,20));
+        panel.add(combo);
+        //Pour ajouter un index : combo.addItem("index");
+
+        //On ajoute un bouton
+        JButton bouton1 = new JButton("LOAD");
+        panel.add(bouton1);
+
+
+        //On ajoute un menu
+        JMenuBar menuBar = new JMenuBar();
+        this.setJMenuBar(menuBar);
+
+        JMenu menu = new JMenu();
+        //menu.add(menuItem);
+        menuBar.add(menu);
+
+        Container contentPane = this.getContentPane();
+        contentPane.add(panel, BorderLayout.CENTER);
     }
 }
 
