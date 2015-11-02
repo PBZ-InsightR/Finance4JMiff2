b16cbfccfd09ae89602bf822e2da580b31ca1c7b
PBZ-InsightR <pbezault@yahoo.fr>
2015-11-02 09:49:18 +0100

    using FXML

--- src/main/java/com/pbz4esilv/gui/Finance4JApp.java
+++ src/main/java/com/pbz4esilv/gui/Finance4JApp.java
@@ -8,23 +8,31 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Finance4JApp extends Application {
+    ComboBox<String> comboBox;
+    Button loadButton;
+
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
 
-        HBox mainPane = new HBox();
+        Pane mainPane = FXMLLoader.load(getClass().getResource("Finance4J.fxml"));
 
-        ComboBox<String> comboBox = new ComboBox<>();
+        HBox hbox = (HBox) mainPane.getChildren().get(0);
+
+        comboBox = (ComboBox<String>) hbox.getChildren().get(0);
         comboBox.getItems().add("AXA");
         comboBox.getItems().add("Allianz");
         comboBox.getItems().add("Bred");
-        mainPane.getChildren().add(comboBox);
 
-        Button loadButton = new Button("LOAD");
-        mainPane.getChildren().add(loadButton);
+
+        loadButton = (Button) hbox.getChildren().get(1);
+        loadButton.setText("LOAD");
 
 
         loadButton.setOnAction(new EventHandler<ActionEvent>() {
