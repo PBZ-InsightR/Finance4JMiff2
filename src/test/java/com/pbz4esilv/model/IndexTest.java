ce2b7d593dd104c8380332c4d23a0702a575cd66
arthur <arthurprvst@yahoo.com>
2015-11-18 22:43:41 +0100

    Merge branch 'master' of https://github.com/PBZ-InsightR/Finance4JMiff2
    # Conflicts:
    #	src/main/java/com/pbz4esilv/model/Index.java

--- src/test/java/com/pbz4esilv/model/IndexTest.java
+++ src/test/java/com/pbz4esilv/model/IndexTest.java
@@ -1,6 +1,19 @@
 package com.pbz4esilv.model;
 
-public class IndexTest
-{
+import  junit.framework.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+public class IndexTest {
+
+    @Test
+    public void testAddEquity(Equity eqt) throws Exception {
+        Index indexTest;
+        indexTest = new Index("test",new Date());
+        indexTest.addEquity(eqt);
+
+        Assert.assertTrue(java.util.ArrayList.isNotEmpty(IndexTest));//verifier si la liste n'est pas vide
+    }
 
 }
