fd3982d5b224cdcbc256aa750b145414f2d814b5
khaled <kh.belili@gmail.com>
2015-11-19 14:29:22 +0100

    Commentaire classe Index.java

--- src/main/java/com/pbz4esilv/model/Equity.java
+++ src/main/java/com/pbz4esilv/model/Equity.java
@@ -24,21 +24,21 @@ public class Equity {
 
     public String getName() {
         return this.name;
-    }
+    }                       //retourne le nom
     public double getPrice() {
         return this.price;
-    }
+    }                     //retourne le prix
     public String getTicker() {
         return this.ticker;
-    }
+    }                   //retourne le symbole
     public double getVolatility() {
         return this.volatility;
-    }
+    }           //retourne la volatilité
     public Date getDateValuation() {
         return this.dateValuation;
-    }
+    }       //retourne la date
     public double getDividend() {
         return this.dividend;
-    }
-    public double getWeight() {return this.weight;}
+    }               //retourne le nombre de dividende
+    public double getWeight() {return this.weight;}                     //retourne le volume
 }
