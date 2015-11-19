b00ba3d337daec1cb03312e8ca154a69ff11bd5e
ChiChi <chironromain@gmail.com>
2015-11-19 15:09:00 +0100

    Correction de testFindEquityByName
    Pour utiliser assertTrue il faut appeler la méthode equals

--- src/test/java/com/pbz4esilv/model/IndexTest.java
+++ src/test/java/com/pbz4esilv/model/IndexTest.java
@@ -8,30 +8,30 @@ import java.util.Date;
 public class IndexTest {
 
     @Test
-    public void testAddEquity(Equity eqt) throws Exception {
+    public void testAddEquity(Equity equity) throws Exception {
         Index indexTest;
         indexTest = new Index("test",new Date());
-        indexTest.addEquity(eqt);
+        indexTest.addEquity(equity);
 
 
         Assert.assertEquals(1,indexTest.getList().size());//verifier si la liste n'est pas vide
     }
-    public void testGetSize()
+    public void testGetSize() throws  Exception
     {
         Index indexTest;
         indexTest=new Index("test",new Date());
         Assert.assertEquals(1,indexTest.getSize());
     }
-    public void testFindEquityByName(String nameEquityToFindTest)
+    public void testFindEquityByName(String nameEquityToFindTest) throws Exception
     {
         Equity eqt;
         eqt = new Equity(nameEquityToFindTest,"test",0.0,0.0,null,0.0,0.0);
         Index indexTest;
         indexTest=new Index("test",new Date());
         indexTest.addEquity(eqt);
-        Assert.assertTrue(indexTest.findEquityByName(nameEquityToFindTest).getName()==nameEquityToFindTest);//on verifie si le nom de l'equity est bien celui ajouté
+        Assert.assertTrue(indexTest.findEquityByName(nameEquityToFindTest).equals(nameEquityToFindTest));//on verifie si le nom de l'equity est bien celui ajouté
     }
-    public void testFindEquityByTicker(String tickerEquityToFind)
+    public void testFindEquityByTicker(String tickerEquityToFind) throws Exception
     {
         Equity eqt;
         eqt = new Equity("test",tickerEquityToFind,0.0,0.0,null,0.0,0.0);
