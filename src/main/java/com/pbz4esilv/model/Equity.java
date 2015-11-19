5d8b2a3e84964eb0459728975f776bb161e9e43b
khaled <kh.belili@gmail.com>
2015-11-19 19:48:54 +0100

    -Correction erreurs compilation Index JAVA -commentaire

--- src/main/java/com/pbz4esilv/model/Equity.java
+++ src/main/java/com/pbz4esilv/model/Equity.java
@@ -1,6 +1,7 @@
 package com.pbz4esilv.model;
 
 import java.util.Date;
+import java.util.Random;
 
 public class Equity {
 
@@ -35,7 +36,7 @@ public class Equity {
     }                   //retourne le symbole
     public double getVolatility() {
         return this.volatility;
-    }           //retourne la volatilité
+    }           //retourne la volatilitï¿½
     public Date getDateValuation() {
         return this.dateValuation;
     }       //retourne la date
@@ -43,4 +44,44 @@ public class Equity {
         return this.dividend;
     }               //retourne le nombre de dividendes
     public double getWeight() {return this.weight;}                     //retourne le volume
+
+
+
+    private double nextNextGaussian;
+    private boolean haveNextNextGaussian = false;
+    synchronized public double nextGaussian() {
+        Random randomno = new Random();
+        if (haveNextNextGaussian) {
+            haveNextNextGaussian = false;
+            return nextNextGaussian;
+        } else {
+            double v1, v2, s;
+            do {
+                v1 = 2 * randomno.nextDouble() - 1; // between -1 and 1
+                v2 = 2 * randomno.nextDouble() - 1; // between -1 and 1
+                s = v1 * v1 + v2 * v2;
+            } while (s >= 1 || s == 0);
+            double multiplier = StrictMath.sqrt(-2 * StrictMath.log(s)/s);
+            nextNextGaussian = v2 * multiplier;
+            haveNextNextGaussian = true;
+            return v1 * multiplier;
+        }
+    }
+
+    // LOI UNIFORME
+    public double loi_uniforme() {
+        Random r = new Random();
+        double i = r.nextInt(100);
+        return i;
+    }
+    // LOI NORMALE
+    public double _loi_normale(double m, double s){
+        double x1,x2,y;
+        x1 = loi_uniforme();
+        x2 = loi_uniforme();
+
+        // methode de Boc-Muller
+        y = Math.pow(-2*Math.log(x1),0.5)*Math.cos(2.* 3.*x2);
+        return m + s*y;
+    }
 }
